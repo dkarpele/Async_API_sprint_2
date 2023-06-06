f612abe07e13b55d724ae231e67b537606838ce4
Denis Karpelevich <dkarpele@redhat.com>
2023-06-06 22:47:55 +0200

    Create TestFilms class Fill the ES with data for all indexes
    Signed-off-by: Denis Karpelevich <dkarpele@redhat.com>

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -5,7 +5,7 @@ from elasticsearch import AsyncElasticsearch
 from elasticsearch.helpers import async_bulk
 
 from tests.functional.settings import settings
-from tests.functional.testdata import es_data
+from tests.functional.testdata import es_data, es_schemas
 
 
 @pytest_asyncio.fixture(scope="session")
@@ -32,21 +32,28 @@ async def session_client():
 
 @pytest_asyncio.fixture
 async def es_write_data(es_client):
-    def get_es_bulk_query(data):
+    def get_es_bulk_query(_index, data):
         doc = []
         for row in data:
             doc.append(
-                {'_index': settings.es_index,
+                {'_index': _index,
                  '_id': row[settings.es_id_field],
                  '_source': row
                  })
         return doc
 
-    bulk_query = get_es_bulk_query(es_data.get_es_data())
+    for index in settings.es_indexes.split():
+        schema = es_schemas.schemas[index]
+        await es_client.indices.create(index=index,
+                                       settings=schema['settings'],
+                                       mappings=schema['mappings'])
 
-    await async_bulk(es_client, bulk_query)
-    await es_client.indices.refresh()
+        bulk_query = get_es_bulk_query(index, es_data.data[index])
+        await async_bulk(es_client, bulk_query)
+        await es_client.indices.refresh()
 
     yield
 
-    await es_client.indices.delete(index=settings.es_index)
+    for index in settings.es_indexes.split():
+        await es_client.indices.flush(index=index)
+        await es_client.indices.delete(index=index)
--- tests/functional/src/test_film.py
+++ tests/functional/src/test_film.py
@@ -2,18 +2,144 @@ import pytest
 
 from tests.functional.settings import settings
 
-expected_answer = {'status': 200,
-                   'length': 50,
-                   'search': 'The Star'}
 
+class TestFilms:
+    @pytest.mark.asyncio
+    async def test_get_all_films(self,
+                                 es_write_data,
+                                 session_client):
+        expected_answer = {'status': 200,
+                           'length': 50,
+                           'title': 'The Star'}
+        url = settings.service_url + '/api/v1/films'
 
-@pytest.mark.asyncio
-async def test_film(es_write_data,
-                    session_client):
-    url = settings.service_url + '/api/v1/films'
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert len(body) == expected_answer['length']
+            assert body[0]['title'] == expected_answer['title']
+            assert list(body[0].keys()) == ['uuid', 'title', 'imdb_rating']
+            assert type(body[0]['imdb_rating']) == float
 
-    async with session_client.get(url) as response:
-        body = await response.json()
-        assert response.status == expected_answer['status']
-        assert len(body) == expected_answer['length']
-        assert body[0]['title'] == expected_answer['search']
+    @pytest.mark.asyncio
+    async def test_get_all_films_genre(self,
+                                       es_write_data,
+                                       session_client):
+        expected_answer = {'status': 200,
+                           'length': 50,
+                           'title': 'The Star'}
+        url = settings.service_url + '/api/v1/films/?genre=Action'
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert len(body) == expected_answer['length']
+            assert body[0]['title'] == expected_answer['title']
+
+        url = settings.service_url + '/api/v1/films/?genre=Music Story'
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert len(body) == expected_answer['length']
+            assert body[0]['title'] == expected_answer['title']
+
+        url = settings.service_url + '/api/v1/films/?genre=music'
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert len(body) == expected_answer['length']
+            assert body[0]['title'] == expected_answer['title']
+
+    @pytest.mark.asyncio
+    async def test_get_all_films_genre_not_exists(self,
+                                                  es_write_data,
+                                                  session_client):
+        expected_answer = {'status': 404,
+                           'detail': 'movies not found'}
+        url = settings.service_url + '/api/v1/films/?genre=Not Exist'
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert body['detail'] == expected_answer['detail']
+
+    @pytest.mark.asyncio
+    async def test_get_all_films_pagination(self,
+                                            es_write_data,
+                                            session_client):
+        expected_answer = {'status': 200,
+                           'page_size': 5}
+        url = settings.service_url + '/api/v1/films/?page_size=5'
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert len(body) == expected_answer['page_size']
+
+        expected_answer = {'status': 200,
+                           'page_size': 60 - 50}
+        url = settings.service_url + '/api/v1/films/?page_number=2'
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert len(body) == expected_answer['page_size']
+
+        expected_answer = {'status': 200,
+                           'page_size': 5}
+        url = settings.service_url + '/api/v1/films/?page_number=4&page_size=5'
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert len(body) == expected_answer['page_size']
+
+    @pytest.mark.asyncio
+    async def test_get_all_films_pagination_negative(self,
+                                                     es_write_data,
+                                                     session_client):
+        expected_answer = {'status': 422,
+                           'type': 'value_error.number.not_ge',
+                           'msg': 'greater than or equal to 1'}
+        url = settings.service_url + '/api/v1/films/?page_size=-5'
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert body['detail'][0]['type'] == expected_answer['type']
+            assert expected_answer['msg'] in body['detail'][0]['msg']
+
+        expected_answer = {'status': 422,
+                           'type': 'value_error.number.not_le',
+                           'msg': 'less than or equal to 500'}
+        url = settings.service_url + '/api/v1/films/?page_size=500000000000000'
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert body['detail'][0]['type'] == expected_answer['type']
+            assert expected_answer['msg'] in body['detail'][0]['msg']
+
+        expected_answer = {'status': 422,
+                           'type': 'value_error.number.not_ge',
+                           'msg': 'greater than or equal to 1'}
+        url = settings.service_url + '/api/v1/films/?page_number=-5'
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert body['detail'][0]['type'] == expected_answer['type']
+            assert expected_answer['msg'] in body['detail'][0]['msg']
+
+        expected_answer = {'status': 422,
+                           'type': 'value_error.number.not_le',
+                           'msg': 'less than or equal to 10000'}
+        url = settings.service_url + '/api/v1/films/?page_number=5000000000000'
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert body['detail'][0]['type'] == expected_answer['type']
+            assert expected_answer['msg'] in body['detail'][0]['msg']
