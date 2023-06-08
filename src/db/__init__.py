34c5b6481afe8c92e4822f61eb33aa4113cb1186
lubovSovina <sovinaliubov@yandex.ru>
2023-06-08 20:12:43 +0300

    Add docs for methods AbstractStorage

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -14,8 +14,25 @@ class AbstractStorage(ABC):
 
     @abstractmethod
     async def get_by_id(self, _id: str, index: str, model) -> Optional:
+        """
+        Абстрактный асинхронный метод для получения данных по id
+        :param _id: строка с id по которому выполняется поиск
+        :param index: строковое название индекса, в котором выполняется поиск
+        :param model: тип модели, в котором возвращаются данные
+        :return: объект типа, заявленного в model
+        """
         ...
 
     @abstractmethod
     async def get_list(self, model, index: str, sort: str, search: dict, page: int, size: int) -> list | None:
+        """
+        Абстрактный асинхронный метод для получения списка данных
+        :param model: тип модели, в котором возвращаются данные
+        :param index: строковое название индекса, в котором выполняется поиск
+        :param sort: строка с названием атрибута, по которому необходима сортировка
+        :param search: словарь с параметрами для поиска, если они необходимы
+        :param page: номер страницы
+        :param size: количество элементов на странице(в списке)
+        :return: список объектов типа model
+        """
         ...
