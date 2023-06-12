76d1cd3ac4636ae460cfdaee3ce9fb15818e0bd5
Denis Karpelevich <dkarpele@redhat.com>
2023-06-12 21:20:02 +0200

    Create Tests for genres
     - Speed up tests by creating data for every class
     - simplify get_by_id fixture
    
    Signed-off-by: Denis Karpelevich <dkarpele@redhat.com>

---
+++ tests/functional/src/test_genre.py
@@ -0,0 +1,176 @@
+import logging
+
+import pytest
+from logging import config as logging_config
+
+from tests.functional.settings import settings
+from tests.functional.utils.logger import LOGGING
+
+# Применяем настройки логирования
+logging_config.dictConfig(LOGGING)
+
+PREFIX = '/api/v1/genres'
+
+
+@pytest.mark.usefixtures('redis_clear_data_before_after',
+                         'es_write_data')
+class TestGenres:
+    @pytest.mark.parametrize(
+        'url, expected_answer',
+        [
+            (
+                    f'{PREFIX}',
+                    {'status': 200, 'length': 3, 'name': 'Fantasy'}
+            ),
+        ]
+    )
+    @pytest.mark.asyncio
+    async def test_get_all_genres(self,
+                                  session_client,
+                                  url,
+                                  expected_answer):
+        url = settings.service_url + url
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert len(body) == expected_answer['length']
+            assert body[0]['name'] == expected_answer['name']
+            assert list(body[0].keys()) == ['uuid', 'name']
+
+
+@pytest.mark.usefixtures('redis_clear_data_before_after', 'es_write_data')
+class TestGenreID:
+    @pytest.mark.asyncio
+    async def test_get_genre_by_id(self,
+                                   session_client,
+                                   get_by_id):
+        _id = await get_by_id(f'{PREFIX}')
+        expected_answer = {'status': 200, 'length': 2, 'name': 'Fantasy'}
+        url = settings.service_url + PREFIX + '/' + _id
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert body['name'] == expected_answer['name']
+            assert len(body) == expected_answer['length']
+            assert body['uuid'] == _id
+            assert list(body.keys()) == ['uuid', 'name']
+
+    @pytest.mark.asyncio
+    async def test_get_genre_id_not_exists(self,
+                                           session_client):
+
+        url = settings.service_url + PREFIX + '/' + 'BAD_ID'
+        expected_answer = {'status': 404}
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert body['detail'] == "BAD_ID not found in genres"
+
+
+class TestGenresRedis:
+    """
+    The idea behind the Test class is:
+    1. Run method `prepare`, that will load data to ES:
+        1. Run HTTP request that will load data to redis
+        2. Remove all data from ES
+    2. Run method `get_from_redis`:
+        1. Run HTTP request that will try to get data from redis
+        2. Show the output
+        3. Upload the data to redis
+    """
+
+    params_list = \
+        [
+            (
+                f'{PREFIX}',
+                {'status': 200, 'length': 3, 'name': 'Fantasy'}
+            )
+        ]
+
+    # This test only adds data to ES, adds data to redis, removes data from ES
+    @pytest.mark.parametrize(
+        'url, expected_answer',
+        params_list
+    )
+    @pytest.mark.asyncio
+    async def test_prepare_data(self,
+                                redis_clear_data_before,
+                                es_write_data,
+                                session_client,
+                                url,
+                                expected_answer):
+        url = settings.service_url + url
+
+        async with session_client.get(url) as response:
+            assert response.status == 200
+
+    # This test DOESN'T add data to ES, but adds data to redis
+    @pytest.mark.parametrize(
+        'url, expected_answer',
+        params_list
+    )
+    @pytest.mark.asyncio
+    async def test_get_from_redis(self,
+                                  redis_clear_data_after,
+                                  session_client,
+                                  url,
+                                  expected_answer):
+        url = settings.service_url + url
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert len(body) == expected_answer['length']
+            assert body[0]['name'] == expected_answer['name']
+            assert list(body[0].keys()) == ['uuid', 'name']
+
+
+class TestGenreIdRedis:
+    """
+    The idea behind the Test class is:
+    1. Run method `prepare`, that will load data to ES:
+        1. Run HTTP request that will load data to redis
+        2. Remove all data from ES
+    2. Run method `get_from_redis`:
+        1. Run HTTP request that will try to get data from redis
+        2. Show the output
+        3. Upload the data to redis
+    """
+
+    # This test only adds data to ES, adds data to redis, removes data from ES
+    @pytest.mark.asyncio
+    async def test_prepare_data(self,
+                                redis_clear_data_before,
+                                es_write_data,
+                                session_client,
+                                get_by_id):
+        # Collect uuid
+        global _id
+        _id = await get_by_id(f'{PREFIX}')
+
+        # Find data by id
+        url = settings.service_url + PREFIX + '/' + _id
+        async with session_client.get(url) as response:
+            assert response.status == 200
+
+    # This test DOESN'T add data to ES, but adds data to redis
+    @pytest.mark.asyncio
+    async def test_get_from_redis(self,
+                                  redis_clear_data_after,
+                                  session_client):
+
+        expected_answer = {'status': 200, 'length': 2, 'name': 'Fantasy'}
+        try:
+            url = settings.service_url + PREFIX + '/' + _id
+        except NameError:
+            logging.error(f"Can't run the test {PREFIX}/UUID with unknown id")
+            assert False
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert body['name'] == expected_answer['name']
+            assert len(body) == expected_answer['length']
+            assert body['uuid'] == _id
+            assert list(body.keys()) == ['uuid', 'name']
