09dbeb86a124efe87cedaa9ed0d7495a33842938
lubovSovina <sovinaliubov@yandex.ru>
2023-06-14 18:26:07 +0300

    Fix tests

--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -74,25 +74,6 @@ class TestPersonSearch:
                     f'{PREFIX}/search?query=jack',
                     {'status': 200, 'length': 1, 'full_name': 'Jack Jones', 'len_films': 50}
             ),
-
-            (
-                    f'{PREFIX}/search?query=Jack&page_number=4&page_size=5000',
-                    {'status': 422,
-                     'type0': 'value_error.number.not_le',
-                     'msg': 'less than or equal to 500'}
-            ),
-            (
-                    f'{PREFIX}/search?query=doesntexist',
-                    {'status': 404, 'detail': 'persons not found'}
-            ),
-            (
-                    f'{PREFIX}/search?query=Jack&page_number=4&page_size=4',
-                    {'status': 404, 'detail': 'persons not found'}
-            ),
-            (
-                    f'{PREFIX}/search?query=',
-                    {'status': 404, 'detail': 'Empty `query` attribute'}
-            ),
         ]
     )
     @pytest.mark.asyncio
@@ -116,6 +97,46 @@ class TestPersonSearch:
                 assert list(body[0].keys()) == ['uuid', 'full_name', 'films']
                 assert type(body[0]['films']) == list
                 assert len(body[0]['films']) == expected_answer['len_films']
+
+    @pytest.mark.asyncio
+    async def test_search_persons_pagination_negative(self,
+                                                      session_client):
+        url = settings.service_url + f'{PREFIX}/search?query=Jack&page_number=4&page_size=5000'
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == 422
+            assert body['detail'][0]['type'] == 'value_error.number.not_le'
+            assert 'less than or equal to 500' in body['detail'][0]['msg']
+
+    @pytest.mark.parametrize(
+        'url, expected_answer',
+        [
+            (
+                    f'{PREFIX}/search?query=doesntexist',
+                    {'status': 404, 'detail': 'persons not found'}
+            ),
+            (
+                    f'{PREFIX}/search?query=Jack&page_number=4&page_size=4',
+                    {'status': 404, 'detail': 'persons not found'}
+            ),
+            (
+                    f'{PREFIX}/search?query=',
+                    {'status': 404, 'detail': 'Empty `query` attribute'}
+            ),
+        ]
+    )
+    @pytest.mark.asyncio
+    async def test_search_persons_not_found(self,
+                                            session_client,
+                                            url,
+                                            expected_answer):
+        url = settings.service_url + url
+
+        async with session_client.get(url) as response:
+            body = await response.json()
+            assert response.status == expected_answer['status']
+            assert body['detail'] == expected_answer['detail']
 
 
 class TestPersonIdRedis:
@@ -166,7 +187,6 @@ class TestPersonIdRedis:
             assert len(body) == expected_answer['length']
             assert body['uuid'] == _id
             assert list(body.keys()) == ['uuid', 'full_name', 'films']
-
 
     @pytest.mark.asyncio
     async def test_get_films_from_redis(self,
