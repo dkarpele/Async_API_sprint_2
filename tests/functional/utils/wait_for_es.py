add58124e59813782f9d82ce8ee9de1715500fc6
lubovSovina <102506320+lubovSovina@users.noreply.github.com>
2023-06-15 15:38:19 +0300

    Add backoff for wait (#32)
    * Add backoff for wait
    
    * Fix import backoff for docker
    
    * Stylistic fixes for backoff
    
    Signed-off-by: Denis Karpelevich <dkarpele@redhat.com>
    
    ---------
    
    Co-authored-by: Denis Karpelevich <dkarpele@redhat.com>

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,26 +1,20 @@
-import logging
-import time
-
 from elasticsearch import Elasticsearch
 from logging import config as logging_config
 
 from utils.logger import LOGGING
 from settings import settings
 
+from utils.backoff import backoff, BackoffError
+
 # Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
 
 
+@backoff(service='ES')
 async def wait_es():
-    print(settings.elastic_host)
     es_client = Elasticsearch(
         hosts=f'http://{settings.elastic_host}:{settings.elastic_port}',
         validate_cert=False,
         use_ssl=False)
-    while True:
-        if es_client.ping(human=True, pretty=True):
-            logging.info(f"ES replied successfully on "
-                         f"http://{settings.elastic_host}:"
-                         f"{settings.elastic_port}")
-            break
-        time.sleep(1)
+    if not es_client.ping():
+        raise BackoffError()
--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,25 +1,19 @@
-import logging
-import time
-
 from redis.asyncio import Redis
 from logging import config as logging_config
 
 from utils.logger import LOGGING
 from settings import settings
 
+from utils.backoff import backoff, BackoffError
 
 # Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
 
 
+@backoff(service='Redis')
 async def wait_redis():
     redis_client = Redis(host=settings.redis_host,
                          port=settings.redis_port,
                          ssl=False)
-    while True:
-        if redis_client.ping():
-            logging.info(f"Redis replied successfully on "
-                         f"http://{settings.redis_host}:"
-                         f"{settings.redis_port}")
-            break
-        time.sleep(1)
+    if not await redis_client.ping():
+        raise BackoffError
